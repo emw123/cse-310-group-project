7f6d5b595d31fd4968a88403c339112b18104178
Joseph Carlson <91073873+Jcarlson4@users.noreply.github.com>
2022-10-19 12:32:06 -0600

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore, collection, getDocs} from 'firebase/firestore';
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAd7ejCkuFATzSxaVB9lWpIC-Seqw2F6M4",
+    authDomain: "mapgamegentlebadgers.firebaseapp.com",
+    projectId: "mapgamegentlebadgers",
+    storageBucket: "mapgamegentlebadgers.appspot.com",
+    messagingSenderId: "846784163575",
+    appId: "1:846784163575:web:505dd6e01f09ba818fadf2",
+    measurementId: "G-QFZEVLZVG6"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+  
+export default db;
