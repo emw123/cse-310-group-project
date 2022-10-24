707efb7e99b7f3e130afcf9ffa59cfc48b020b30
Emma <87395798+emw123@users.noreply.github.com>
2022-10-24 12:51:07 -0600

    Country Read

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import db from "./firebase"
 
@@ -10,12 +10,41 @@ import Sidebar from './components/Sidebar'
 import Map from './components/Map'
 
 function App() {
-  useEffect(()=> load(), [])
+  const [country, setCountry] = useState("");
+  useEffect(()=>{
+    db.collection("country").onSnapshot((snapshot) => {
+      setCountry(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+    console.log({ country });
+  }, load(), [])
+
+  const submit = (e) => {
+    e.preventDefault();
+    db.collection("country").add({
+      name: country,
+    });
+  
+    setCountry("");
+  };
+
   return (
     <div className='App'>
       <Sidebar/>
       <Map/>
+      <input
+          type="text"
+          placeholder="country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+      <button onClick={submit}>Submit</button>
     </div>
+    
   );
 }
 
