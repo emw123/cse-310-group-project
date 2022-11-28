28a31bd8e6ad8fc094b6203eb743ab9910c3443b
Lucas Nethercott <luke@nethercott.com>
2022-11-28 13:04:16 -0700

    code cleanup

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,32 +1,21 @@
-import '../App.css';
-import React from 'react';
-import { MapContainer, TileLayer, useMapEvent, Polygon} from 'react-leaflet'
+import { MapContainer, TileLayer, Polygon } from 'react-leaflet'
 import features from '../map/map.js'
 
-function Map(props) {
+export default function Map(props) {
   const countryPolygons=features.map( features=>
     <Polygon 
       pathOptions={{ color: 'clear' }} 
       positions={(features.geometry.coordinates[0]) } 
-      eventHandlers={({
-        mouseover: () => {
-          props.setCountry(features.properties.name)
-          // console.log(features.properties.name)
-        },
-      })}
-    />);
+      eventHandlers={({mouseover: () => {props.setCountry(features.properties.name)}})}
+    />)
 
   return (
-
     <MapContainer center={[35, -90]} zoom={3} scrollWheelZoom={true}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    
-    {countryPolygons}
-  </MapContainer>
-  );
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {countryPolygons}
+    </MapContainer>
+  )
 }
-  
-export default Map;
