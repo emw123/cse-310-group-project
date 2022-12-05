525f8b2eff6cfa350b2d7e2635fac07ea267991e
Lucas Nethercott <luke@nethercott.com>
2022-12-05 12:40:27 -0700

    map almost rendering right

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,22 @@
 import { MapContainer, TileLayer, Polygon } from 'react-leaflet'
 import features from '../map/map.js'
 
-export default function Map(props) {
-  const countryPolygons=features.map( features=>
-    <Polygon 
-      pathOptions={{ color: 'clear' }} 
-      positions={(features.geometry.coordinates[0]) } 
-      eventHandlers={({mouseover: () => {props.setCountry(features.properties.name)}})}
-    />)
+function reverseChildLists(list){
+    let reversed=[]
+    for (let i = 0; i < list.length; i++) {
+      reversed.push(list[i].reverse())
+    }
+    return reversed
+}
 
+export default function Map(props) {
+  const countryPolygons=features.map(features=>
+    <Polygon
+      pathOptions={{ color: 'clear' }} 
+      positions={(reverseChildLists(features.geometry.coordinates[0]))}
+      eventHandlers={({mouseover: () => {props.setCountry(features.properties.name)}})}
+    />
+  )
   return (
     <MapContainer center={[35, -90]} zoom={3} scrollWheelZoom={true}>
       <TileLayer
