aa63abaf400f06c574dd7135b1dbfe7ff9e55012
TicTacToe333 <aidencedwards@gmail.com>
2022-12-05 14:07:35 -0700

    Changed the map file back to GeoJson, and the name of the country will appear as a popup when clicked on
    The onEachEvent function in the Map.js file creates the popups, so any attempt to pass the country name to the main file should start there

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,32 @@
-import { MapContainer, TileLayer, Polygon } from 'react-leaflet'
-import features from '../map/map.js'
+import '../App.css'
+import React from 'react'
+import { MapContainer, TileLayer, GeoJSON} from 'react-leaflet'
+import map from '../map/map.json'
 
-export default function Map(props) {
-  const countryPolygons=features.map( features=>
-    <Polygon 
-      pathOptions={{ color: 'clear' }} 
-      positions={(features.geometry.coordinates[0]) } 
-      eventHandlers={({mouseover: () => {props.setCountry(features.properties.name)}})}
-    />)
+
+function onEachFeature(feature, layer) {
+  layer.bindPopup(feature.properties.name)
+}
+
+
+
+
+function Map() {
 
   return (
     <MapContainer center={[35, -90]} zoom={3} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {countryPolygons}
-    </MapContainer>
-  )
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      noWrap = {true}
+    />
+    
+    <GeoJSON
+    data = {map.features}
+    style = {{fillOpacity: '0'}}
+    onEachFeature={onEachFeature}
+    />
+  </MapContainer>
+  );
 }
+export default Map;
